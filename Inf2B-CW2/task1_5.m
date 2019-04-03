106ce21f33e2cba2c79cc43750de41e8cb1fbb0a
adelliinaa <33165052+adelliinaa@users.noreply.github.com>
2019-04-03 19:01:21 +0100

    Last Update 1_5

--- Inf2B-CW2/task1_5.m
+++ Inf2B-CW2/task1_5.m
@@ -1,9 +1,11 @@
 function task1_5(X, Ks) 
 
-for i = 1 : length(Ks)
-    k = Ks(i);
-    [C, idx,SSE] = my_kMeansClustering(X, k, X(1:k, :), 50);
-    save(sprintf("task1_5_c_%d.mat", k), "C");
-    save(sprintf("task1_5_idx_%d.mat", k), "idx");
-    save(sprintf("task1_5_SSE_%d.mat", k), "SSE");
+for k = 1 : size(Ks,2)
+    
+    [C, idx,SSE] = my_kMeansClustering(X, Ks(k), X(1:Ks(k), :), 100);
+    save(sprintf("task1_5_c_%d.mat", Ks(k)), "C");
+    save(sprintf("task1_5_idx_%d.mat", Ks(k)), "idx");
+    save(sprintf("task1_5_SSE_%d.mat", Ks(k)), "SSE");
+    %print(figure(k),sprintf('task1_5_SSE_class%d.pdf', Ks(k)),'-dpdf');
+    
 end
