77716c85a43184c4d5bc2d226ac3ac1f3cadd8c0
adelliinaa <33165052+adelliinaa@users.noreply.github.com>
2019-04-08 11:09:20 +0100

    LAST UPDATE Task1_3

--- Inf2B-CW2/task1_3.m
+++ Inf2B-CW2/task1_3.m
@@ -1,29 +1,24 @@
 function [EVecs, EVals, CumVar, MinDims] = task1_3(X)
+	    %compute the PCA 
+	    [m,d] = size(X);
+	    [EVecs, EVals] = comp_pca(X);    
+	    
+	    save('task1_3_evecs.mat','EVecs');
+	    save('task1_3_evals.mat','EVals');
+	    
+	    %compute the cumulative variance
+	    CumVar = cumsum(EVals);
+        figure
+	    plot([1:d], CumVar)
 
-	[EVecs,EVals]=comp_pca(X);
-	
-	%variances=calc_Variances(EVecs);
-    CumVar=cumsum(EVals);
-	
-    MinDims=zeros(4,1);
-	total=size(CumVar,2);
-	figure
-    plot((0:783), CumVar);
-    
-	for i=1:total
-	    if CumVar(i)>=0.7*CumVar(total) && MinDims(1)==0
-	        MinDims(1)=i;
+	    MinDims = zeros(4, 1);
+	    TotalVar = sum(EVals); 
+	    ratio = [0.7 0.8 0.9 0.95];
+	    for (i=1:4)
+	        MinDims(i) = sum(CumVar < (ratio(i) * TotalVar)) + 1;
         end
-	    if CumVar(i)>=0.8*CumVar(total)  && MinDims(2)==0
-	        MinDims(2)=i;
-        end 
-	    if CumVar(i)>=0.9*CumVar(total)  && MinDims(3)==0
-	        MinDims(3)=i;
-        end    	    
-	    if CumVar(i)>=0.95*CumVar(total)  && MinDims(4)==0
-	        MinDims(4)=i;
-	        break
-	    end
+	    save('task1_3_cumvar.mat','CumVar');
+	    save('task1_3_mindims.mat','MinDims');
+	    
 	end
 	
-	end
