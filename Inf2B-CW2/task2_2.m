a77b3dc263dcc70b525a9e98ad29100e76d9c50b
adelliinaa <33165052+adelliinaa@users.noreply.github.com>
2019-04-08 12:21:05 +0100

    Another One - Last Update task2_2

--- Inf2B-CW2/task2_2.m
+++ Inf2B-CW2/task2_2.m
@@ -1,37 +1,22 @@
-function Dmap = task2_2(X, Y, k, MAT_evecs, MAT_evals, posVec, nbins)
+function Dmap = task2_2(Xtrain, Ytrain, k, MAT_evecs, MAT_evals, posVec, nbins)
 
-	    load(MAT_evecs,'EVecs');
-	    load(MAT_evals,'EVals');
-	    
-	%% Initialisation
-	    Dmap = zeros(nbins*nbins, 1);
-	    [M D] = size(X);
-	    plot = zeros(nbins*nbins, D);
-	    
-	    %% initialise the 2D plot plane in 784-D vector space
-	    sd_X = sqrt(EVals(1,:)); sd_Y = sqrt(EVals(2,:));
-	    mu = ( EVecs'*(myMean(X)-posVec)' )';
-	    mu_X = mu(:,1);mu_Y = mu(:,2);
-	    
-	    Xplot = linspace(mu_X-5*sd_X, mu_X+5*sd_X, nbins)';
-	    Yplot = linspace(mu_Y-5*sd_Y, mu_Y+5*sd_Y, nbins)';
-	    [Xv, Yv] = meshgrid(Xplot, Yplot);
-	    plot(:,1:2) = [Xv(:) Yv(:)];
-	    
-	    %% Apply pca to the 2D plane to convert it from principle component
-	    %  coordinate system to the original coordinate system 
-	    plot = bsxfun(@plus, (EVecs' \ plot')', posVec);
-	    
-        %% Perform knn classification in the original coordinate system
-	    Dmap = run_knn_classifier(X, Y, plot, [k]);
-	    Dmap = reshape(Dmap, nbins, nbins);
-	    
-	    %% 
-	    colors = random_colours(10);
-	    figure;
-	    [CC,h] = contourf(Xplot(:), Yplot(:), Dmap);
-	    set(h,'LineColor','none');
-	    colormap(colors);
+	MAT_evecs2 = load(MAT_evecs); mevec = MAT_evecs2.EVecs;
+	MAT_evals2 = load(MAT_evals); meval = MAT_evals2.EVals;
 	
-	%     save(sprintf('task2_2_dmap_%d.mat', k), 'Dmap');
-	endnd
+	m = sum(Xtrain)/size(Xtrain,1);
+	Xplot = linspace(m(1)-5*sqrt(meval(1)), m(1)+5*sqrt(meval(1)), nbins)';
+	Yplot = linspace(m(2)-5*sqrt(meval(2)), m(2)+5*sqrt(meval(2)), nbins)';
+	[Xv, Yv] = meshgrid(Xplot, Yplot);
+	grid = (mevec(:,1:2))*[Xv(:), Yv(:)]' + posVec';
+	gridX = grid';
+
+	[Dmap] = run_knn_classifier(Xtrain, Ytrain, gridX,k);
+
+	[CC,h] = contourf(Xplot(:), Yplot(:), flip(flip(reshape(Dmap, length(Xplot), length(Yplot)),2)));
+	set(h,'LineColor','none');
+	
+	save(sprintf('task2_2_dmap_%d.mat',k),'Dmap');
+	
+	end
+	
+	
