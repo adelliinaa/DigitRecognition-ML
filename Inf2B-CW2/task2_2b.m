e9778cb3ba2287a826a7de6613bcb17100c79dee
adelliinaa <33165052+adelliinaa@users.noreply.github.com>
2019-04-08 12:19:56 +0100

    Another One - Last 2.2 Update

--- Inf2B-CW2/task2_2b.m
+++ Inf2B-CW2/task2_2b.m
@@ -1,40 +1,22 @@
-function Dmap = task2_2b(Xtrain, Ytrain, k, MAT_evecs, MAT_evals, posVec, nbins)
+function Dmap = task2_2(Xtrain, Ytrain, k, MAT_evecs, MAT_evals, posVec, nbins)
+
+	MAT_evecs2 = load(MAT_evecs); mevec = MAT_evecs2.EVecs;
+	MAT_evals2 = load(MAT_evals); meval = MAT_evals2.EVals;
 	
-	load(MAT_evecs,'EVecs');
-	load(MAT_evals,'EVals');
-	numSamples = size(Xtrain,1);
-	mean = sum(Xtrain,1)/numSamples; % row vectors
-	Y1 = EVecs(:,1); %column vectors
-	Y2 = EVecs(:,2);
-	numGridPoints = nbins*nbins;
-	% The means are the projection of the mean onto the 2 principal components
-	meanY1 = mean*Y1;
-	meanY2 = mean*Y2;
-	varY1 = EVals(1,:);
-	varY2 = EVals(2,:);
-	Y1plot = linspace(meanY1-5*sqrt(varY1),meanY2+5*sqrt(varY1), nbins)';
-	Y2plot = linspace(meanY1-5*sqrt(varY2),meanY2+5*sqrt(varY2), nbins)';
-	% Obtain the grid vectors for the two dimensions
-	[Y1v Y2v] = meshgrid(Y1plot, Y2plot);
-	grid2D = [Y1v(:), Y2v(:)]; % Concatenate to get a 2-D point.			  
-	%Dmap = grid2D;
-	% Revert projection into D-Space 
-	projectedGridPoints = zeros(numGridPoints,784);
-	for i=1:numGridPoints
-	    projectedGridPoints(i,:) = (EVecs*padarray(grid2D(i,:),[0 782], 'post')'+posVec')'; % Orthogonality of EVecs implies that the inverse of EVecs is EVecs'
-	end
-	
-	%k-NN Classification
-	Ypreds = run_knn_classifier(Xtrain, Ytrain, projectedGridPoints, k)
-	classesOfPoints = Ypreds(:,1);
-	    
-	Dmap = permute(reshape(classesOfPoints,nbins,nbins),[2 1])
-	
-	% This function will draw the decision boundaries
-	figure
-	[CC,h] = contourf(Y1plot(:), Y2plot(:),reshape(classesOfPoints, length(Y1plot), length(Y2plot)));
+	m = sum(Xtrain)/size(Xtrain,1);
+	Xplot = linspace(m(1)-5*sqrt(meval(1)), m(1)+5*sqrt(meval(1)), nbins)';
+	Yplot = linspace(m(2)-5*sqrt(meval(2)), m(2)+5*sqrt(meval(2)), nbins)';
+	[Xv, Yv] = meshgrid(Xplot, Yplot);
+	grid = (mevec(:,1:2))*[Xv(:), Yv(:)]' + posVec';
+	gridX = grid';
+
+	[Dmap] = run_knn_classifier(Xtrain, Ytrain, gridX,k);
+
+	[CC,h] = contourf(Xplot(:), Yplot(:), flip(flip(reshape(Dmap, length(Xplot), length(Yplot)),2)));
 	set(h,'LineColor','none');
-	end
 	
+	save(sprintf('task2_2_dmap_%d.mat',k),'Dmap');
+	
+	end
 	
 	
